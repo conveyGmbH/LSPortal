34991475325eaea32cf4e52a9deb1161b429e0a3
Alexandra858 <31213752+Alexandra858@users.noreply.github.com>
2017-09-28 15:42:17 +0200

    SketchList fragment eingefügt

---
+++ fragments/sketchList/sketchListService.js
@@ -0,0 +1,30 @@
+﻿// service for page: sketchList
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("SketchList", {
+        _sketchlistView: {
+            get: function () {
+                return AppData.getFormatView("KontaktNotiz", 20504, false);
+            }
+        },
+        sketchlistView: {
+            select: function (complete, error, restriction) {
+                Log.call(Log.l.trace, "SketchList.");
+                var ret = SketchList._sketchlistView.select(complete, error, restriction, {
+                    ordered: true,
+                    orderAttribute: "KontaktNotizVIEWID"
+                });
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        }
+    });
+})();
+
+
