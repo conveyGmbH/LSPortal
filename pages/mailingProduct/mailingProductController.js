51234693409c474ff20aa7a7d8337912ba11b0da
Alexander Rößger <alexander@convey.de>
2017-11-24 14:50:07 +0100

    Product Mail Structure

---
+++ pages/mailingProduct/mailingProductController.js
@@ -0,0 +1,104 @@
+﻿// controller for page: MailingProduct
+/// <reference path="~/www/lib/WinJS/scripts/base.js" />
+/// <reference path="~/www/lib/WinJS/scripts/ui.js" />
+/// <reference path="~/www/lib/convey/scripts/appSettings.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+/// <reference path="~/www/lib/convey/scripts/appbar.js" />
+/// <reference path="~/www/scripts/generalData.js" />
+/// <reference path="~/www/pages/mailingProduct/mailingProductService.js" />
+
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("MailingProduct", {
+        Controller: WinJS.Class.derive(Application.Controller, function Controller(pageElement, commandList) {
+            Log.call(Log.l.trace, "MailingProduct.Controller.");
+            Application.Controller.apply(this, [pageElement, {}, commandList]);
+
+            var that = this;
+            
+            var listView = pageElement.querySelector("#mailingProductList.listview");
+
+            // Then, do anything special on this page
+            this.eventHandlers = {
+                clickBack: function (event) {
+                    Log.call(Log.l.trace, "Contact.Controller.");
+                    if (WinJS.Navigation.canGoBack === true) {
+                        WinJS.Navigation.back(1).done();
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                clickOk: function (event) {
+                    Log.call(Log.l.trace, "Info.Controller.");
+                    if (WinJS.Navigation.canGoBack === true) {
+                        WinJS.Navigation.back(1).done( /* Your success and error handlers */);
+                    } else {
+                        Application.navigateById("start", event);
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                clickChangeUserState: function (event) {
+                    Log.call(Log.l.trace, "Info.Controller.");
+                    Application.navigateById("userinfo", event);
+                    Log.ret(Log.l.trace);
+                },
+                clickGotoPublish: function (event) {
+                    Log.call(Log.l.trace, "Info.Controller.");
+                    Application.navigateById("publish", event);
+                    Log.ret(Log.l.trace);
+                }
+            }
+
+            var loadData = function () {
+                Log.call(Log.l.trace, "MailingProduct.");
+                AppData.setErrorMsg(that.binding);
+                var ret = new WinJS.Promise.as().then(function () {
+                    return MailingProduct.MAILERZEILENView.select(function (json) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        Log.print(Log.l.trace, "MailingProduct: success!");
+                        // startContact returns object already parsed from json file in response
+                        if (json && json.d) {
+                            var results = json.d.results;
+                            that.mailingZeile = new WinJS.Binding.List(results);
+
+                            if (listView && listView.winControl) {
+                                // add ListView dataSource
+                                listView.winControl.itemDataSource = that.mailingZeile.dataSource;
+                            }
+                        } else {
+                            that.reportingColumn = null;
+                            if (listView && listView.winControl) {
+                                // add ListView dataSource
+                                listView.winControl.itemDataSource = null;
+                            }
+                        }
+                    }, function (errorResponse) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        AppData.setErrorMsg(that.binding, errorResponse);
+                    },
+                        {
+                            
+                        }
+                    );
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            };
+            this.loadData = loadData;
+
+            // Finally, wire up binding
+            that.processAll().then(function () {
+                Log.print(Log.l.trace, "Binding wireup page complete");
+                return that.loadData();
+            }).then(function () {
+                Log.print(Log.l.trace, "Data loaded");
+            });
+            Log.ret(Log.l.trace);
+        }, {
+            mailingZeile: null
+        })
+    });
+})();
---
+++ pages/mailingProduct/mailingProductService.js
@@ -0,0 +1,102 @@
+﻿// service for page: MailingProduct
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("MailingProduct", {
+        _MaildokumentView: {
+            get: function () {
+                return AppData.getFormatView("Maildokument");
+            }
+        },
+        MaildokumentView: {
+            select: function (complete, error, restriction) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MaildokumentView.select(complete,error,restriction,
+                    {
+                        ordered: true
+                    });
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            getNextUrl: function (response) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MaildokumentView.getNextUrl(response);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            selectNext: function (complete, error, response, nextUrl) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MaildokumentView.selectNext(complete, error, response, nextUrl);
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            update: function (complete, error, recordId, viewResponse) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MaildokumentView.update(complete, error, recordId, viewResponse);
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        },
+        _MAILERZEILENView: {
+            get: function () {
+                return AppData.getFormatView("MailerZeilen", 20514);
+            }
+        },
+        MAILERZEILENView: {
+            select: function (complete, error, restriction) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MAILERZEILENView.select(complete, error, restriction,
+                    {
+                    ordered: true,
+                    orderAttribute: "MAILERZEILENVIEWID"
+                    });
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            getNextUrl: function (response) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MAILERZEILENView.getNextUrl(response);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            selectNext: function (complete, error, response, nextUrl) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MAILERZEILENView.selectNext(complete, error, response, nextUrl);
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        }
+        /*
+        update: function (complete, error, recordId, viewResponse) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MAILERZEILENView.update(complete, error, recordId, viewResponse);
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            insert: function (complete, error) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MAILERZEILENView.insert(complete, error, {
+                    MAILERZEILENVIEWID: 0
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            deleteRecord: function (complete, error, recordId) {
+                Log.call(Log.l.trace, "MailingProduct.");
+                var ret = MailingProduct._MAILERZEILENView.deleteRecord(complete, error, recordId);
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        */
+    });
+})();
