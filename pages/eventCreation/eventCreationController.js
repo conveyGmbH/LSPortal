e9bc3d58f05933e034fc74a26d5e81662dad3a4e
Alexander Rößger <alexander@convey.de>
2018-01-24 14:07:34 +0100

    Event Creation Structure

---
+++ pages/eventCreation/eventCreationController.js
@@ -0,0 +1,34 @@
+﻿// controller for page: eventCreation
+/// <reference path="~/www/lib/WinJS/scripts/base.js" />
+/// <reference path="~/www/lib/WinJS/scripts/ui.js" />
+/// <reference path="~/www/lib/convey/scripts/appSettings.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+/// <reference path="~/www/lib/convey/scripts/appbar.js" />
+/// <reference path="~/www/lib/convey/scripts/pageController.js" />
+/// <reference path="~/www/scripts/generalData.js" />
+/// <reference path="~/www/pages/eventCreation/eventCreationService.js" />
+
+(function () {
+    "use strict";
+    WinJS.Namespace.define("EventCreation", {
+        Controller: WinJS.Class.derive(Application.Controller, function Controller(pageElement, commandList) {
+            Log.call(Log.l.trace, "EventCreation.Controller.");
+            Application.Controller.apply(this, [pageElement, {
+                
+            }, commandList]);
+
+            var that = this;
+
+            
+
+            that.processAll().then(function () {
+                Log.print(Log.l.trace, "Binding wireup page complete");
+                return that.loadData();
+            }).then(function () {
+                Log.print(Log.l.trace, "Data loaded");
+                AppBar.notifyModified = true;
+            });
+            Log.ret(Log.l.trace);
+        })
+    });
+})();
---
+++ pages/eventCreation/eventCreationService.js
@@ -0,0 +1,12 @@
+﻿// service for page: eventCreation
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("EventCreation", {
+       
+    });
+})();
