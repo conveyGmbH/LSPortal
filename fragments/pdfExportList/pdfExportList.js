b3e22016160cd2d4befeaa49d1d98a2e15718c51
Alexander Rößger <alexander@convey.de>
2018-06-13 17:27:21 +0200

    PdfExportbutton Grundgerüst

---
+++ fragments/pdfExportList/pdfExportList.js
@@ -0,0 +1,70 @@
+﻿// For an introduction to the Page Control template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232511
+/// <reference path="~/www/lib/WinJS/scripts/base.js" />
+/// <reference path="~/www/lib/WinJS/scripts/ui.js" />
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/navigator.js" />
+/// <reference path="~/www/fragments/pdfExportList/pdfExportListController.js" />
+
+(function () {
+    "use strict";
+
+    var fragmentName = Application.getFragmentPath("PdfExportList");
+
+    Fragments.define(fragmentName, {
+        // This function is called whenever a user navigates to this fragment. It
+        // populates the fragment elements with the app"s data.
+        ready: function (element, options) {
+            Log.call(Log.l.trace, fragmentName + ".");
+            // TODO: Initialize the fragment here.
+            this.inResize = 0;
+            this.prevWidth = 0;
+            this.prevHeight = 0;
+
+            this.controller = new PdfExportList.Controller(element, options);
+
+            Log.ret(Log.l.trace);
+        },
+
+        unload: function () {
+            Log.call(Log.l.trace, fragmentName + ".");
+            // TODO: Respond to navigations away from this page.
+
+            this.controller = null;
+            Log.ret(Log.l.trace);
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            var ret = null;
+            var that = this;
+            /// <param name="element" domElement="true" />
+            Log.call(Log.l.u1, fragmentName + ".");
+            // TODO: Respond to changes in viewState.
+            if (element && !that.inResize) {
+                that.inResize = 1;
+                ret = WinJS.Promise.timeout(0).then(function () {
+                    var empList = element.querySelector("#pdfExportList.listview");
+                    if (empList && empList.style) {
+                        var contentarea = element.querySelector(".contentarea");
+                        if (contentarea) {
+                            var width = contentarea.clientWidth;
+                            var height = contentarea.clientHeight;
+                            if (width !== that.prevWidth) {
+                                that.prevWidth = width;
+                                empList.style.width = width.toString() + "px";
+                            }
+                            if (height !== that.prevHeight) {
+                                that.prevHeight = height;
+                                empList.style.height = height.toString() + "px";
+                            }
+                        }
+                    }
+                    that.inResize = 0;
+                });
+            }
+            Log.ret(Log.l.u1);
+            return ret;
+        }
+    });
+})();
---
+++ fragments/pdfExportList/pdfExportListService.js
@@ -0,0 +1,49 @@
+﻿// service for page: reportingList
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("PdfExportList", {
+        _pdfExportListView: {
+            get: function () {
+                return AppData.getFormatView("", 0);
+            }
+        },
+        pdfExportListView: {
+            select: function (complete, error) {
+                Log.call(Log.l.trace, "PdfExportList.");
+                var ret = PdfExportList._pdfExportListView.select(complete, error, {
+                    LanguageID: AppData.getLanguageId()
+                }, {
+                        ordered: true,
+                        orderAttribute: "",
+                        desc: true
+                    });
+                // this will return a promise to controller
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            getNextUrl: function (response) {
+                Log.call(Log.l.trace, "PdfExportList.pdfExportListView.");
+                var ret = PdfExportList._pdfExportListView.getNextUrl(response);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            getResults: function () {
+                Log.call(Log.l.trace, "PdfExportList.pdfExportListView.");
+                var ret = PdfExportList._pdfExportListView.results;
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            getMap: function () {
+                Log.call(Log.l.trace, "PdfExportList.pdfExportListView.");
+                var ret = PdfExportList._pdfExportListView.map;
+                Log.ret(Log.l.trace);
+                return ret;
+            }
+        }
+    });
+})();
