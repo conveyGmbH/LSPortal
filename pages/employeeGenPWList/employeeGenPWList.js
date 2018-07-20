d80cd969fd2bdeabc3043e116a6723001fd025f4
Alexander Rößger <alexander@convey.de>
2018-07-20 12:07:44 +0200

    EmployeeGenPWList export button icon changed

--- pages/employeeGenPWList/employeeGenPWList.js
+++ pages/employeeGenPWList/employeeGenPWList.js
@@ -51,7 +51,7 @@
             // add page specific commands to AppBar
             var commandList = [
                 { id: "clickBack", label: getResourceText("command.backward"), tooltip: getResourceText("tooltip.backward"), section: "primary", svg: "navigate_left" },
-                { id: "clickExport", label: getResourceText("command.export"), tooltip: getResourceText("tooltip.export"), section: "primary", svg: "download" }
+                { id: "clickExport", label: getResourceText("command.export"), tooltip: getResourceText("tooltip.export"), section: "primary", svg: "arrow_barrier_down" }
             ];
 
             this.controller = new EmployeeGenPWList.Controller(element, commandList);
