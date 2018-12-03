947fa9c370886a2e48ee3f225c2e387f14c1f9e3
Alexander Rößger <alexander@convey.de>
2018-12-03 16:30:03 +0100

    Support site
    Bitte in Group 8  hinzufügen

---
+++ pages/support/supportController.js
@@ -0,0 +1,79 @@
+﻿// controller for page: support
+/// <reference path="~/www/lib/WinJS/scripts/base.js" />
+/// <reference path="~/www/lib/WinJS/scripts/ui.js" />
+/// <reference path="~/www/lib/convey/scripts/appSettings.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+/// <reference path="~/www/lib/convey/scripts/appbar.js" />
+/// <reference path="~/www/lib/convey/scripts/pageController.js" />
+/// <reference path="~/www/scripts/generalData.js" />
+/// <reference path="~/www/pages/support/supportService.js" />
+
+(function () {
+    "use strict";
+    WinJS.Namespace.define("Support", {
+        Controller: WinJS.Class.derive(Application.Controller, function Controller(pageElement, commandList) {
+            Log.call(Log.l.trace, "Support.Controller.");
+            Application.Controller.apply(this, [pageElement, {
+                
+            }, commandList]);
+
+            var that = this;
+
+            this.eventHandlers = {
+                clickBack: function (event) {
+                    Log.call(Log.l.trace, "Support.Controller.");
+                    if (WinJS.Navigation.canGoBack === true) {
+                        WinJS.Navigation.back(1).done();
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                clickChangeUserState: function (event) {
+                    Log.call(Log.l.trace, "Support.Controller.");
+                    Application.navigateById("userinfo", event);
+                    Log.ret(Log.l.trace);
+                },
+                clickGotoPublish: function (event) {
+                    Log.call(Log.l.trace, "Support.Controller.");
+                    Application.navigateById("publish", event);
+                    Log.ret(Log.l.trace);
+                }
+            };
+
+            this.disableHandlers = {
+                clickBack: function () {
+                    if (WinJS.Navigation.canGoBack === true) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                },
+                clickGotoPublish: function () {
+                    return true;
+                }
+            };
+
+            var conveyUrl = function() {
+                if (AppData.getLanguageId() === 1031) {
+                    window.open('https://www.convey.de/kontakt/');
+                    WinJS.Navigation.back(1).done();
+                } else {
+                    window.open('https://www.convey.de/contact-2/');
+                    WinJS.Navigation.back(1).done();
+                }
+            }
+            this.conveyUrl = conveyUrl;
+
+            that.processAll().then(function () {
+                Log.print(Log.l.trace, "Binding wireup page complete");
+                return that.conveyUrl();
+            }).then(function () {
+                Log.print(Log.l.trace, "Data loaded");
+                AppBar.notifyModified = true;
+            });
+            Log.ret(Log.l.trace);
+        })
+    });
+})();
+
+
+
---
+++ pages/support/supportService.js
@@ -0,0 +1,27 @@
+﻿// service for page: support
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Support", {
+        _templateView: {
+            get: function () {
+                return AppData.getFormatView("VIEW hier eintragen", 0);
+            }
+        },
+        templateView: {
+            select: function (complete, error, restriction) {
+                Log.call(Log.l.trace, "questionView.");
+                var ret = Support._templateView.select(complete, error, restriction);
+                Log.ret(Log.l.trace);
+                return ret;
+            },
+            defaultValue: {
+               
+            }
+        }
+    });
+})();
