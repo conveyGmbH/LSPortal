8ff1b5a94f54875d6427b02f76615823eb6e7587
hung <quanghungbui1@gmail.com>
2019-02-14 09:30:56 +0100

    bug qrcode - wrong counter

--- pages/employeeGenPWList/employeeGenPWListController.js
+++ pages/employeeGenPWList/employeeGenPWListController.js
@@ -196,7 +196,7 @@
                                             for (var j = 0; j < barcodeImages.length; j++) {
                                                 var barcodeImage = barcodeImages[j];
                                                 if (barcodeImage) { //barcodeImage.barcode
-                                                    var value = "#li:" + that.employeePWListdata.getAt(j).Login + "/" + that.employeePWListdata.getAt(j).GenPassword; // barcodeImage.barcode.substring(9, 13)
+                                                    var value = "#li:" + that.employeePWListdata.getAt(i).Login + "/" + that.employeePWListdata.getAt(i).GenPassword; // barcodeImage.barcode.substring(9, 13)
                                                     var qrcodeViewer = document.createElement("div");
                                                     WinJS.Utilities.addClass(qrcodeViewer, "userinfo-qrcode");
                                                     $(qrcodeViewer).qrcode({
