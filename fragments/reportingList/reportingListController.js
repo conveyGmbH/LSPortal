8e9062889ce3b92630f589b3f03527af6f84069e
Alexander Rößger <alexander@convey.de>
2019-10-02 13:24:56 +0200

    Blocked OLELetterID 32 "Frangenstatistik"

--- fragments/reportingList/reportingListController.js
+++ fragments/reportingList/reportingListController.js
@@ -29,13 +29,17 @@
 
             var createEventHandler = function(id) {
                 Log.call(Log.l.trace, "ReportingList.Controller.", "id=" + id);
-                eventHandlers["clickOLELetterID" + id] = function (event) {
-                    Log.call(Log.l.trace, "ReportingList.Controller.");
-                    if (event && event.currentTarget) {
-                        event.currentTarget.value = id;
-                        AppBar.handleEvent('click', 'clickExport', event);
-                    }
-                    Log.ret(Log.l.trace);
+                if (id === 32) {
+                    Log.call(Log.l.trace, "ReportingList.Controller.", "blocked id=" + id);
+                } else {
+                    eventHandlers["clickOLELetterID" + id] = function (event) {
+                        Log.call(Log.l.trace, "ReportingList.Controller.");
+                        if (event && event.currentTarget) {
+                            event.currentTarget.value = id;
+                            AppBar.handleEvent('click', 'clickExport', event);
+                        }
+                        Log.ret(Log.l.trace);
+                    }  
                 }
                 Log.ret(Log.l.trace);
             }
@@ -65,9 +69,21 @@
             }
             this.disableList = disableList;
 
+            var resultConverter = function (item, index) {
+                item.index = index;
+                if (item.OLELetterID === 32) {
+                    Log.print(Log.l.trace, "OLELetterID 32 blocked!");
+                } else {
+                    that.reportingItem.push(item);
+                }
+               
+            }
+            this.resultConverter = resultConverter;
+
             var loadData = function () {
                 Log.call(Log.l.trace, "ReportingList.");
                 AppData.setErrorMsg(that.binding);
+                that.reportingItem = new WinJS.Binding.List();
                 var ret = new WinJS.Promise.as().then(function () {
                     return ReportingList.analysisListView.select(function (json) {
                         // this callback will be called asynchronously
@@ -76,8 +92,9 @@
                         // startContact returns object already parsed from json file in response
                         if (json && json.d && json.d.results && json.d.results.length > 0) {
                             var results = json.d.results;
-
-                            that.reportingItem = new WinJS.Binding.List(results);
+                            results.forEach(function (item, index) {
+                                that.resultConverter(item, index);
+                            });
                             if (listView && listView.winControl) {
                                 // add ListView dataSource
                                 listView.winControl.itemDataSource = that.reportingItem.dataSource;
@@ -86,12 +103,16 @@
                             var commandList = [];
                             for (var i = 0; i < results.length; i++) {
                                 var id = results[i].OLELetterID;
-                                commandList.push({
-                                    id: "clickOLELetterID" + id,
-                                    label: results[i].Text,
-                                    section: "secondary"
-                                });
-                                createEventHandler(id);
+                                if (results[i].OLELetterID === 32) {
+                                    Log.print(Log.l.trace, "OLELetterID 32 blocked!");
+                                } else {
+                                    commandList.push({
+                                        id: "clickOLELetterID" + id,
+                                        label: results[i].Text,
+                                        section: "secondary"
+                                    });
+                                    createEventHandler(id);
+                                }
                             }
                             that.eventHandlers = eventHandlers;
                             that.commandList = commandList;
