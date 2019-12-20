28ca001d4fed58807cbf453d183b02ef3ae20830
Alexander Rößger <alexander@convey.de>
2019-12-20 17:03:11 +0100

    NoData site

---
+++ fragments/startNoData/startNoDataController.js
@@ -0,0 +1,49 @@
+﻿// controller for page: StartNoData
+/// <reference path="~/www/lib/WinJS/scripts/base.js" />
+/// <reference path="~/www/lib/WinJS/scripts/ui.js" />
+/// <reference path="~/www/lib/convey/scripts/appSettings.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+/// <reference path="~/www/lib/convey/scripts/fragmentController.js" />
+/// <reference path="~/www/scripts/generalData.js" />
+/// <reference path="~/www/fragments/startNoData/startNoDataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("StartNoData", {
+        Controller: WinJS.Class.derive(Fragments.Controller, function Controller(fragmentElement, options) {
+            Log.call(Log.l.trace, "StartNoData.Controller.");
+            Fragments.Controller.apply(this, [fragmentElement, {
+
+            }, options]);
+
+            var that = this;
+            
+            var resultConverter = function (item, index) {
+                item.index = index;
+            }
+            this.resultConverter = resultConverter;
+
+            var loadData = function () {
+                Log.call(Log.l.trace, "StartNoData.");
+                AppData.setErrorMsg(that.binding);
+                var ret = new WinJS.Promise.as().then(function () {
+                    
+                });
+                Log.ret(Log.l.trace);
+                return ret;
+            };
+            this.loadData = loadData;
+
+            that.processAll().then(function () {
+                Log.print(Log.l.trace, "Binding wireup page complete");
+                return that.loadData();
+            }).then(function () {
+                Log.print(Log.l.trace, "Data loaded");
+            });
+            Log.ret(Log.l.trace);
+        }, {
+
+            })
+    });
+})();
