471ddd917b9f300d31b09a19454d21c0027183cf
Gilbert Schwaab <gilbert@convey.de>
2020-06-12 13:25:59 +0200

    New Home screen initial version

---
+++ pages/home/homeController.js
@@ -0,0 +1,155 @@
+﻿// controller for page: home
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/appSettings.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+/// <reference path="~/www/lib/convey/scripts/appbar.js" />
+/// <reference path="~/www/lib/convey/scripts/pageController.js" />
+/// <reference path="~/www/scripts/generalData.js" />
+/// <reference path="~/www/pages/home/homeService.js" />
+
+/*
+ Structure of states to be set from external modules:
+ {
+    networkState: newNetworkstate:
+ }
+*/
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Home", {
+        Controller: WinJS.Class.derive(Application.Controller, function Controller(pageElement, commandList) {
+            Log.call(Log.l.trace, "Home.Controller.");
+
+            Application.Controller.apply(this, [pageElement, {
+            }, commandList]);
+
+            var that = this;
+
+
+            var listView = pageElement.querySelector("#homeActions.listview");
+            /*
+            if (listView && listView.winControl) {
+                listView.winControl.itemDataSource = null;
+                if (listView.style) {
+                    listView.style.visibility = "hidden";
+                }
+            }
+            var layout = null;
+             */
+
+            this.dispose = function() {
+                if (listView && listView.winControl) {
+                    listView.winControl.itemDataSource = null;
+                }
+            }
+
+            // define handlers
+            this.eventHandlers = {
+                clickBack: function (event) {
+                    Log.call(Log.l.trace, "Home.Controller.");
+                    if (WinJS.Navigation.canGoBack === true) {
+                        WinJS.Navigation.back(1).done( /* Your success and error handlers */);
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                clickChangeUserState: function (event) {
+                    Log.call(Log.l.trace, "Home.Controller.");
+                    Application.navigateById("userinfo", event);
+                    Log.ret(Log.l.trace);
+                },
+                onItemInvoked: function (eventInfo) {
+                    Log.call(Log.l.trace, "TimeList.Controller.");
+                    if (eventInfo && eventInfo.detail) {
+                        Log.print(Log.l.trace, "itemIndex=" + eventInfo.detail.itemIndex);
+                        var item = Home.actionsView.getAt(eventInfo.detail.itemIndex);
+                        if (item && item.page) {
+                            Application.navigateById(item.page, event);
+                        }
+                    }
+                    Log.ret(Log.l.trace);
+                },
+                onLoadingStateChanged: function (eventInfo) {
+                    Log.call(Log.l.trace, "Home.Controller.");
+                    if (listView && listView.winControl) {
+                        var i;
+                        Log.print(Log.l.trace, "onLoadingStateChanged called loadingState=" + listView.winControl.loadingState);
+                        if (listView.winControl.loadingState === "itemsLoading") {
+                            /*
+                            if (!layout) {
+                                layout = Application.HomeLayout.ActionsLayout;
+                                listView.winControl.layout = { type: layout };
+                            }
+                             */
+                        } else if (listView.winControl.loadingState === "itemsLoaded") {
+                            Colors.loadSVGImageElements(listView, "action-image", 40, "#ffffff", "name", function (svgInfo) {
+                                if (svgInfo.element && svgInfo.element.parentNode) {
+                                    var actionItem = svgInfo.element.parentNode.parentNode;
+                                    if (actionItem && actionItem.style && actionItem.style.visibility !== "visible") {
+                                        WinJS.Promise.timeout(Math.floor(Math.random() * 150) + 20).then(function() {
+                                            actionItem.style.visibility = "visible";
+                                            return WinJS.UI.Animation.enterPage(actionItem);
+                                        });
+                                    }
+                                }
+                                return WinJS.Promise.as();
+                            });
+                        } else if (listView.winControl.loadingState === "complete") {
+                            //
+                        }
+                    }
+                    Log.ret(Log.l.trace);
+                }
+            };
+
+            this.disableHandlers = {
+                clickBack: function() {
+                    if (WinJS.Navigation.canGoBack === true) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            };
+
+            if (listView) {
+                // add ListView event handler
+                this.addRemovableEventListener(listView, "loadingstatechanged", this.eventHandlers.onLoadingStateChanged.bind(this));
+                this.addRemovableEventListener(listView, "iteminvoked", this.eventHandlers.onItemInvoked.bind(this));
+            }
+
+            // finally, load the data
+            that.processAll().then(function() {
+                Log.print(Log.l.trace, "Binding wireup page complete, now load data");
+                if (listView && listView.winControl) {
+                    // no list selection
+                    if (listView.winControl.selectionMode !== WinJS.UI.SelectionMode.none) {
+                        listView.winControl.selectionMode = WinJS.UI.SelectionMode.none;
+                    }
+                    // invoke
+                    if (listView.winControl.tapBehavior !== WinJS.UI.TapBehavior.invokeOnly) {
+                        listView.winControl.tapBehavior = WinJS.UI.TapBehavior.invokeOnly;
+                    }
+                    // add ListView dataSource
+                    listView.winControl.itemDataSource = Home.actionsView.dataSource;
+                }
+                return WinJS.Promise.as();
+            }).then(function () {
+                Log.print(Log.l.trace, "Data loaded");
+                if (!Application.pageframe.splashScreenDone) {
+                    WinJS.Promise.timeout(20).then(function () {
+                        return Application.pageframe.hideSplashScreen();
+                    });
+                }
+            });
+            Log.ret(Log.l.trace);
+        })
+    });
+})();
+
+
+
+
+
+
+
---
+++ pages/home/homeService.js
@@ -0,0 +1,33 @@
+﻿// service for page: start
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("Home", {
+        _actions: [
+            { page: "esStaffAdministration", imageName: "keys" },
+            { page: "voucherAdministration", imageName: "Gutschein1.1" },
+            { page: "resourcesAdministration", imageName: "user_headset" },
+            { page: "start", imageName: "home" }
+        ],
+        _actionsList: null,
+        actionsView: {
+            get: function () {
+                if (!Home._actionsList) {
+                    var list = [];
+                    Home._actions.forEach(function (item, index) {
+                        item.title = getResourceText(item.page + ".title");
+                        item.comment = getResourceText(item.page + ".comment");
+                        item.imageBkgColor = Colors.navigationColor;
+                        list.push(item);
+                    });
+                    Home._actionsList = new WinJS.Binding.List(list);
+                }
+                return Home._actionsList;
+            }
+        }
+    });
+})();
