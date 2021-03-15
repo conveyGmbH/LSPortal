9caa285048fa0c9ef23780f22dc8edb0ed87d735
Gilbert Schwaab <gilbert@convey.de>
2021-03-15 15:43:09 +0100

    Update eventGenSettingsController.js

--- pages/eventGenSettings/eventGenSettingsController.js
+++ pages/eventGenSettings/eventGenSettingsController.js
@@ -327,7 +327,7 @@
             this.eventHandlers = {
                 clickBack: function (event) {
                     Log.call(Log.l.trace, "Contact.Controller.");
-                    if (WinJS.Navigation.canGoBack === true) {
+                    if (!Application.showMaster() && WinJS.Navigation.canGoBack === true) {
                         WinJS.Navigation.back(1).done();
                     }
                     Log.ret(Log.l.trace);
