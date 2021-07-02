504e7bbb42c37f061b2dbce2be99156b588af25c
Gilbert Schwaab <gilbert@convey.de>
2021-07-02 20:21:56 +0200

    Update home.js

--- pages/home/home.js
+++ pages/home/home.js
@@ -20,6 +20,7 @@
             Log.call(Log.l.trace, pageName + ".");
             // TODO: Initialize the page here.
             var contentarea = element.querySelector(".contentarea");
+            var homeActions = element.querySelector("#homeActions");
             if (contentarea && contentarea.style) {
                 if (Colors.isDarkTheme) {
                     var bkg = Colors.hex2rgb(Colors.tileBackgroundColor);
@@ -28,8 +29,14 @@
                     bkgHsv.v /= 4;
                     var darkBkg = Colors.hsv2rgb(bkgHsv);
                     contentarea.style.backgroundColor = Colors.rgb2hex(darkBkg);
+                    if (homeActions && homeActions.style) {
+                        homeActions.style.backgroundColor = Colors.rgb2hex(darkBkg);
+                    }
                 } else {
                     contentarea.style.backgroundColor = Colors.tileBackgroundColor;
+                    if (homeActions && homeActions.style) {
+                        homeActions.style.backgroundColor = Colors.tileBackgroundColor;
+                    }
                 }
             }
 
