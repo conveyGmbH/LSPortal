8a8460db650564ea57184d3a0508404a47305e0e
convey-alexander <alexander@convey.de>
2022-03-28 14:26:58 +0200

    #6966: New application Ticket Limits added

---
+++ pages/ticketLimits/ticketLimitsService.js
@@ -0,0 +1,45 @@
+﻿// service for page: MailingProduct
+/// <reference path="~/www/lib/convey/scripts/strings.js" />
+/// <reference path="~/www/lib/convey/scripts/logging.js" />
+/// <reference path="~/www/lib/convey/scripts/dataService.js" />
+
+(function () {
+    "use strict";
+
+    WinJS.Namespace.define("TicketLimits", {
+            _CRVeranstaltungBucketView: {
+                get: function () {
+                    return AppData.getFormatView("CRVeranstaltungBucket", 20660);
+                }
+            },
+            _CRVeranstaltungBucketTable: {
+                get: function () {
+                    return AppData.getFormatView("CRVeranstaltungBucket", 0);
+                }
+            },
+            CRVeranstaltungBucketView: {
+                select: function (complete, error, restriction) {
+                    Log.call(Log.l.trace, "MailingProduct.");
+                    var ret = TicketLimits._CRVeranstaltungBucketView.select(complete, error, restriction, { ordered: true });
+                    // this will return a promise to controller
+                    Log.ret(Log.l.trace);
+                    return ret;
+                },
+                update: function (complete, error, recordId, viewResponse) {
+                    Log.call(Log.l.trace, "MailingProduct.");
+                    var ret = TicketLimits._CRVeranstaltungBucketTable.update(complete, error, recordId, viewResponse);
+                    // this will return a promise to controller
+                    Log.ret(Log.l.trace);
+                    return ret;
+                },
+                defaultValue: {
+                    CRVeranstaltungBucketVIEWID: 0,
+                    VeranstaltungID: 0,
+                    ESBucketID: 0,
+                    LocalLimit: 0,
+                    LocalFree: 0,
+                    BucketText: ""
+                }
+            }
+    });
+})();
