7522db8cfcb2b47b05408979bfcebfd6e80d5ab4
hung <quanghungbui1@gmail.com>
2023-12-04 12:33:19 +0100

    #7812

--- pages/mandatory/mandatory.js
+++ pages/mandatory/mandatory.js
@@ -54,6 +54,9 @@
                 { id: "clickBack", label: getResourceText("command.backward"), tooltip: getResourceText("tooltip.backward"), section: "primary", svg: "navigate_left" },
                 { id: "clickOk", label: getResourceText("command.ok"), tooltip: getResourceText("tooltip.ok"), section: "primary", svg: "navigate_check", key: WinJS.Utilities.Key.enter }
             ];
+            commandList = [
+                { id: 'clickBack', label: getResourceText('command.backward'), tooltip: getResourceText('tooltip.backward'), section: 'primary', svg: 'navigate_left' }
+            ];
             this.controller = new Mandatory.Controller(element, commandList);
             if (this.controller.eventHandlers) {
                 // general event listener for hardware back button, too!
