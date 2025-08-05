aef5e7496a2eee51fc52ccf65895d2b7fe600d04
Hung <quanghungbui1@gmail.com>
2025-08-05 16:09:36 +0200

    #8022

--- pages/account/accountController.js
+++ pages/account/accountController.js
@@ -374,6 +374,8 @@
                         that.binding.dataLogin.Password, 
 
                         function setDBPassword(dbPassword) { 
+                            AppData._persistentStates.odata.password = dbPassword;
+                            Application.pageframe.savePersistentStates();
                             that.binding.dataLogin.Password = dbPassword;
                         },
                         Application.language, 
